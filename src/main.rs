1ff89f3d3899926f9f087d2f6d9b421e3e683816
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-11 17:53:58 +0300

    Read a blob attempt 37

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::prelude::*;
 use flate2::read::ZlibDecoder;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+   // println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
      let args: Vec<String> = env::args().collect();
