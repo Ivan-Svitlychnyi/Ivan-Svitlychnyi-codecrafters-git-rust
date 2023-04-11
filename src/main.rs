c7ffdae7771686efa52631167707b7585017c03e
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-11 17:00:58 +0300

    Read a blob attempt 27

--- src/main.rs
+++ src/main.rs
@@ -37,8 +37,7 @@ fn main() {
             z.read_to_string(&mut s).unwrap();
 
             let chars : Vec<char> = s.chars().collect();
-            let s = chars[8..].to_vec().iter().cloned().collect::<String>();
-
+            let s = chars[8..].iter().collect::<String>();
             print!("{}", s);
         
      }
