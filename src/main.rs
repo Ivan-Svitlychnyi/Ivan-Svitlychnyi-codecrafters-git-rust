25bc81c8600b75fee6d8001ef76be30bc3903620
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-11 13:23:33 +0300

    Read a blob attempt 2

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 use std::io::prelude::*;
-use flate2::read::GzDecoder;
-
+use std::io;
+use flate2::read::ZlibDecoder;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
      let args: Vec<String> = env::args().collect();
+     println!("{:?}", args);
      if args[1] == "init" {
          fs::create_dir(".git").unwrap();
          fs::create_dir(".git/objects").unwrap();
@@ -18,16 +19,25 @@ fn main() {
          fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
          println!("Initialized git directory")
 
-     } else if args.contains(&"cat-file".to_string()) && args.contains(&"-p".to_string()) {
+     } //else if args.contains(&"cat-file".to_string()) && args.contains(&"-p".to_string()) {
+       else if args[2] =="cat-file" && args[3]=="-p" {
 
-    let mut d = GzDecoder::new(".git/objects".as_bytes());
-    let mut s = String::new();
-    d.read_to_string(&mut s).unwrap();
-    print!("{}", s);
-
-
-        // println!("unknown command: {}", args[1])
+            let mut new_path = ".git/objects/".to_string();
+            new_path.push_str(&format!("{}", args[4]));
+         
+            let git_data = fs::read(new_path).unwrap();
+            let mut z = ZlibDecoder::new(&git_data[..]);
+            let mut s = String::new();
+            z.read_to_string(&mut s).unwrap();
+            print!("{}", s);
+        
      }
+        else{
+         println!("unknown command: {}", args[1])  
+        }
+
+      
+     
   
 
 }
