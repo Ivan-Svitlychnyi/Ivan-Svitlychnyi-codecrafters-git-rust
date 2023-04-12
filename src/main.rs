c73cdc094faff423facff6670d8179224d183fc2
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-12 13:19:58 +0300

    Read a blob attempt 39

--- src/main.rs
+++ src/main.rs
@@ -22,43 +22,32 @@ fn main() {
         println!("Initialized git directory")
 
     } else if args[1] == "cat-file" && args[2] == "-p" {
-        
+
         let chars: Vec<char> = args[3].chars().collect();
         let sub_dir = chars[..2].iter().collect::<String>();
         let sha_num = chars[2..].iter().collect::<String>();
+        let full_path = format!(".git/objects/{}/{}", sub_dir, sha_num);
 
-        let new_path = &format!(".git/objects/{}/{}", sub_dir, sha_num);
+        print!("{}", read_git_object(&full_path));
 
-        let git_data = fs::read(new_path).unwrap();
-        let mut git_data = ZlibDecoder::new(&git_data[..]);
-
-        let mut s_git_data = String::new();
-        git_data.read_to_string(&mut s_git_data).unwrap();
-
-        let s_chars: Vec<char> = s_git_data.chars().collect();
-        let s = s_chars[8..].iter().collect::<String>();
-
-        print!("{}", s);
     } else {
         println!("unknown command: {}", args[1])
     }
 }
 
-// fn read_git_objects(args: &[String]) -> String {
 
-//     let chars : Vec<char> = args[3].chars().collect();
-//     let sub_dir = chars[..2].to_vec().iter().cloned().collect::<String>();
-//     let sha_num = chars[2..].to_vec().iter().cloned().collect::<String>();
+ fn read_git_object(git_path: &String) -> String {
 
-//     let mut new_path = ".git/objects/".to_string();
-//     new_path.push_str(&format!("{}/{}",sub_dir, sha_num));
+    let git_data = fs::read(git_path).unwrap();
+    let mut git_data = ZlibDecoder::new(&git_data[..]);
 
-//      let git_data = fs::read(new_path).unwrap();
-//      let mut z = ZlibDecoder::new(&git_data[..]);
-//      let mut s = String::new();
-//      z.read_to_string(&mut s).unwrap();
+    let mut s_git_data = String::new();
+    git_data.read_to_string(&mut s_git_data).unwrap();
 
-//      let chars : Vec<char> = s.chars().collect();
-//      let s = chars[8..].iter().filter(|c| **c != '\n').collect::<String>();
+    let git_data_chars: Vec<char> = s_git_data.chars().collect();
+    
+    let git_data = git_data_chars[8..].iter().filter(|c| **c != '\n').collect::<String>();
 
-// }
+    git_data
+
+ }
