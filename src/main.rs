2a02bcb72606cca87522edbc3135ae1f46cfe1d2
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-12 19:25:19 +0300

    Write a blob attempt 14

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
         let file_data = fs::read(args[3].to_string()).unwrap();
         let metadata = fs::metadata(args[3].to_string()).unwrap().len();
         println!("metadata: {:?}", &metadata);
-        let header = format!("blob {}00", metadata);
+        let header = format!("blob {}", metadata);
       
         let store = header + &format!("{:?}",file_data );
 
