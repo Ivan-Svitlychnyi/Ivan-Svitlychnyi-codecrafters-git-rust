f6168e8e9a31b0f8963d204de1096e2d955f3000
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-18 19:19:34 +0300

    Write tree 25

--- src/main.rs
+++ src/main.rs
@@ -150,7 +150,7 @@ fn write_tree(file_path: &String) -> Result<String, io::Error> {
     for dir in entries {
         let path_name = dir.as_os_str().to_str().unwrap();
        // let path_name = dir.as_path().to_str().unwrap();
-        println!("path_name: {}", path_name);
+       // println!("path_name: {}", path_name);
         let mode;
         let sha_file;
 
