92bda2b520fb2360689e026ecbf3ba3332c83ab7
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-18 19:28:52 +0300

    Write tree 26

--- src/main.rs
+++ src/main.rs
@@ -171,10 +171,10 @@ fn write_tree(file_path: &String) -> Result<String, io::Error> {
 
           // println!("file out: {:?}", &sha_file);
         }
-       // println!("file out: {:?}", &sha_file);
+        println!("sha_file: {:?}", &sha_file);
         sha_out = sha_out + (&format!("{mode} {path_name}\x00{}", &sha_file.unwrap()));
         
-       // println!("file out: {:?}", &sha_out);
+        println!("sha_out: {:?}", &sha_out);
 
     }
 
