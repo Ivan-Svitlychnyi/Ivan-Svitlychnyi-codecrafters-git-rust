3bf30b5f65aef93d372e0736e9d08c233b4c1f24
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-18 18:07:21 +0300

    Write tree 8

--- src/main.rs
+++ src/main.rs
@@ -168,9 +168,9 @@ fn write_tree(file_path: &String) -> Result<String, io::Error> {
 
            //println!("file out: {:?}", &sha_file);
         }
-        println!("file out: {:?}", &sha_file);
+       // println!("file out: {:?}", &sha_file);
         sha_out = sha_out + (&format!("{mode} {path_name}\x00{}", &sha_file.unwrap()));
-        
+        println!("file out: {:?}", &sha_out);
     }
 
     let res = write_hash_object(sha_out.into_bytes(), "tree");
