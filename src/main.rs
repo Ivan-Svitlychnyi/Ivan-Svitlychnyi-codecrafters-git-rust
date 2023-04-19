4b9056551ebc51e1e43fe14bf4a0f4d2ea3dcfaf
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-19 12:39:11 +0300

    Write tree 32

--- src/main.rs
+++ src/main.rs
@@ -161,14 +161,14 @@ fn write_tree(file_path: &String) -> Result<String, io::Error> {
         }
 
         if dir.is_dir() {
-         println!("dir: {}", path_name);
+        // println!("dir: {}", path_name);
             mode = "40000";
             sha_file = write_tree(&String::from_str(path_name).unwrap());
 
         } else if dir.is_file() {
            
             mode = "100644";
-            println!("file: {}",  path_name);
+          //  println!("file: {}",  path_name);
 
             let file_data = fs::read( &path_name).unwrap();
 
@@ -176,14 +176,14 @@ fn write_tree(file_path: &String) -> Result<String, io::Error> {
           // println!("file out: {:?}", &sha_file);
         }
         else {
-            println!("else!!!!!!!!!!!!!!!!!!!!!!!!!!");
+           // println!("else!!!!!!!!!!!!!!!!!!!!!!!!!!");
             sha_file = Ok("".to_owned());
         }
        // println!("sha_file: {:?}", &sha_file);
     
         sha_out += &format!("{mode} {path_name}\x00{}", sha_file.unwrap());
         
-        println!("sha_out: {:?}", sha_out);
+       // println!("sha_out: {:?}", sha_out);
 
     }
     let res = write_hash_object(sha_out.into_bytes(), "tree");
