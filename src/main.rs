65dce77b6483d08e7ad3028df9b20d66986e15a3
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-24 13:26:50 +0300

    Create commit 4

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ use std::env;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
-use std::str::FromStr;
 use std::str;
+use std::str::FromStr;
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
@@ -26,23 +26,19 @@ fn main() {
         // println!("{}", )
     } else if args[1] == "cat-file" && args[2] == "-p" {
         print!("{}", read_git_object(&args[3]).unwrap());
-
     } else if args[1] == "hash-object" && args[2] == "-w" {
         let file_data = fs::read(args[3].to_string()).unwrap();
         let (_, sha1_out) = write_hash_object(file_data, "blob").unwrap();
-        println!("hash-object in: {}",sha1_out);
-
+        println!("hash-object in: {}", sha1_out);
     } else if args[1] == "ls-tree" && args[2] == "--name-only" {
         let result = read_tree(&args[3]).unwrap();
         for s in result {
             println!("{}", s);
         }
-
     } else if args[1] == "write-tree" {
         let (_, sha1_out) = write_tree(&".".to_string()).unwrap();
         print!("{}", sha1_out);
     } else if args[1] == "commit-tree" {
-    
         print!("{}", create_commit(&args).unwrap());
     } else {
         println!("unknown command: {:#?}", args)
@@ -83,18 +79,11 @@ fn read_git_object(git_path: &String) -> Result<String, io::Error> {
 
     Ok(git_data)
 }
-fn write_hash_object(file_data: Vec<u8>, file_type: &str) -> Result<(Vec<u8>,String), io::Error> {
-     #[allow(unsafe_code)]
-    let store = 
-        
-     format!(
-        "{file_type} {}\x00{}",
-        file_data.len(),
-        
-        unsafe {
-         String::from_utf8_unchecked(file_data)
-         }
-    );
+fn write_hash_object(file_data: Vec<u8>, file_type: &str) -> Result<(Vec<u8>, String), io::Error> {
+    #[allow(unsafe_code)]
+    let store = format!("{file_type} {}\x00{}", file_data.len(), unsafe {
+        String::from_utf8_unchecked(file_data)
+    });
 
     let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
 
@@ -103,7 +92,7 @@ fn write_hash_object(file_data: Vec<u8>, file_type: &str) -> Result<(Vec<u8>,Str
 
     let mut hasher = Sha1::new();
     hasher.update(store.as_bytes());
-    
+
     let result = hasher.finalize();
 
     let hex_result = hex::encode(&result[..]);
@@ -112,13 +101,11 @@ fn write_hash_object(file_data: Vec<u8>, file_type: &str) -> Result<(Vec<u8>,Str
 
     let sub_dir_path = format!(".git/objects/{}/", sub_dir);
 
-
     let full_path = format!("{sub_dir_path}{}", sha_num);
 
     fs::create_dir(sub_dir_path)?;
     fs::write(full_path, compressed)?;
     Ok((result.to_vec(), hex_result))
-
 }
 
 fn read_tree(file_path: &String) -> Result<Vec<String>, io::Error> {
@@ -151,70 +138,62 @@ fn read_tree(file_path: &String) -> Result<Vec<String>, io::Error> {
     Ok(result)
 }
 
-fn write_tree(file_path: &String) -> Result<(Vec<u8>,String), io::Error>{
-
+fn write_tree(file_path: &String) -> Result<(Vec<u8>, String), io::Error> {
     let mut sha_out: String = "".to_string();
-
 
     let mut entries = fs::read_dir(file_path)
         .unwrap()
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()
         .unwrap();
-  
+
     entries.sort();
 
-    
-    
     for dir in entries {
-     let mode;
-       
-        
+        let mode;
+
         let path_name = dir.to_str().unwrap();
-      //  println!("dir: {}", path_name);
-        
+        //  println!("dir: {}", path_name);
 
         if path_name == "./.git" {
-            continue;      
+            continue;
         }
         let sha_file;
         if dir.is_dir() {
-        
             mode = "40000";
-           (sha_file, _) = write_tree(&String::from_str(path_name).unwrap()).unwrap();
-
-        
-
-        } else /*if dir.is_file()*/ {
-           
+            (sha_file, _) = write_tree(&String::from_str(path_name).unwrap()).unwrap();
+        } else
+        /*if dir.is_file()*/
+        {
             mode = "100644";
-
 
             let file_data = fs::read(&path_name).unwrap();
 
-        (sha_file, _) = write_hash_object(file_data, "blob").unwrap();
+            (sha_file, _) = write_hash_object(file_data, "blob").unwrap();
         }
-         
-        #[allow(unsafe_code)]
-        let s = unsafe {String::from_utf8_unchecked(sha_file)};
-        sha_out += &format!("{mode} {}\x00{}",dir.file_name().unwrap().to_str().unwrap(), s);
 
+        #[allow(unsafe_code)]
+        let s = unsafe { String::from_utf8_unchecked(sha_file) };
+        sha_out += &format!(
+            "{mode} {}\x00{}",
+            dir.file_name().unwrap().to_str().unwrap(),
+            s
+        );
     }
     let res = write_hash_object(sha_out.into_bytes(), "tree");
     res
 }
 
-
 fn create_commit(args: &[String]) -> Result<String, io::Error> {
-
     let (tree_sha, parent_commit_sha, data) = (&args[2], &args[4], &args[6]);
-    
+
     let user_metadata = "author Admin <admin@example.com> 1652217488 +0300\ncommitter Name <committer@example.com> 1652224514 +0300".to_string();
-    
-    let content = format!("tree {tree_sha}\nparent {parent_commit_sha}\n{user_metadata}\n\n{data}\n");
-    
+
+    let content =
+        format!("tree {tree_sha}\nparent {parent_commit_sha}\n{user_metadata}\n\n{data}\n");
+
     //println!("content: {:?}", &content);
-    let (_, sha )= write_hash_object(content.into_bytes(), "commit")?;
-    
+    let (_, sha) = write_hash_object(content.into_bytes(), "commit")?;
+
     Ok(sha)
-    }
+}
