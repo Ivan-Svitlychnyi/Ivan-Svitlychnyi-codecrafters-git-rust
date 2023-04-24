e4d4fb1b353d5788bf53b0bd5357077b2ef9fa32
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-24 18:14:23 +0300

    Clone 9

--- src/main.rs
+++ src/main.rs
@@ -291,7 +291,6 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
     // println!("The bytes: {:?}", &buffer[..n]);
     //println!("res = {:?}", res.bytes());
 
-    
 //-2-------------------------------------------------------------------------------
 
 
