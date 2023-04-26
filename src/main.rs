bc4a979f1ee0cb90ff904d56fc6f1a7c48f2c5f1
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-26 13:33:48 +0300

    Clone 15

--- src/main.rs
+++ src/main.rs
@@ -241,7 +241,7 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
             let tup = c.split(" ").enumerate();
           
             for (num, value) in tup {
-                if /*num == 0 ||*/ num >= 4 {
+                if num == 0 || num >= 4 {
 
                     pack_hash += value;
 
@@ -249,6 +249,8 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
             }
         }
     }
+    pack_hash = String::from(&pack_hash[4..]);
+    
     println!("pack_hash = {}", pack_hash);
     let post_url = url.to_owned() + "/git-upload-pack";
 
