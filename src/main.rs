9903b7d41690d5d75f0eacb11d75ce42e618af41
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-28 20:06:56 +0300

    Clone 21

--- src/main.rs
+++ src/main.rs
@@ -481,7 +481,7 @@ fn identify(delta: &[u8], base: String) -> String {
             let mut offset_bytes = String::new();
            // let mut offset_bytes:[u8; 8] = [0;8];
            let mut n = length;
-            for _ in 2..length{
+            for _ in 0..length{
                 n -= 1;
                 let b = offset_key >> n & 1;
 
@@ -512,7 +512,7 @@ fn identify(delta: &[u8], base: String) -> String {
             let mut len_bytes = String::new();
           // let mut len_bytes:[u8; 8] = [0;8];
           let mut n = length;
-            for _ in 2..length{
+            for _ in 0..length{
                 n -=1;
                 let b = len_key >> n & 1;
 
