38463cac330cb3bf6b4adbdcc902ef3c096a055a
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-28 19:09:09 +0300

    Clone 20

--- src/main.rs
+++ src/main.rs
@@ -478,11 +478,11 @@ fn identify(delta: &[u8], base: String) -> String {
             let length = (offset_key.count_ones() + offset_key.count_zeros()) as usize;
             println!("length: {:?}", &length);
 
-            let offset_key =  offset_key.reverse_bits();
+            let offset_key =  offset_key.reverse_bits()>>2;
 
             //let mut offset_bytes = String::new();
             let mut offset_bytes:[u8; 8] = [0;8];
-            for n in 0..length  {
+            for n in 0..length -2 {
                 
                 let b = offset_key >> n & 1;
 
@@ -509,11 +509,11 @@ fn identify(delta: &[u8], base: String) -> String {
             let length = (len_key.count_ones() + len_key.count_zeros()) as usize;
             println!("  length key: {:?}", &length);
 
-            let len_key = len_key.reverse_bits();
+            let len_key = len_key.reverse_bits() >>2;
 
           //  let mut len_bytes = String::new();
            let mut len_bytes:[u8; 8] = [0;8];
-            for n in 0..length {
+            for n in 0..length - 2{
 
                 let b = len_key >> n & 1;
 
