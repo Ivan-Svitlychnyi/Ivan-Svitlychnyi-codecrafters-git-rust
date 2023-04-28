4ff8557cba85ccdee9020f95ab9c6be88eb70aee
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-28 13:15:29 +0300

    Clone 20

--- src/main.rs
+++ src/main.rs
@@ -302,11 +302,14 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
             if obj_type <7 {
                 let mut git_data = ZlibDecoder::new(&data_bytes[seek..]);
 
+                println!("git_data");
+
                 let mut s_git_data = String::new();
 
-                git_data.read_to_string(&mut s_git_data)?;
+                git_data.read_to_string(&mut s_git_data).unwrap();
 
                 let data_type = ["", "commit", "tree", "blob","", "tag","ofs_delta"];
+
       
                 let mut obj_write_data = format!("{} {}\0", data_type[obj_type], &s_git_data.len());
                 println!("obj_write_data if: {:?}", obj_write_data);
