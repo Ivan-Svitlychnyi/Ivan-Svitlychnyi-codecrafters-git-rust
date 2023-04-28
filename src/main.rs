451a41219a37fdfcc0b94fe4a2d8d2b8625afde2
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-04-28 20:08:48 +0300

    Clone 21

--- src/main.rs
+++ src/main.rs
@@ -518,7 +518,6 @@ fn identify(delta: &[u8], base: String) -> String {
                 if b == 1 {
                     len_bytes += &delta[seek].to_string();
                   // len_bytes[n] = delta[seek];
-
                     println!("len_bytes delta[seek]{}", delta[seek]);
                     seek += 1
                } else {
