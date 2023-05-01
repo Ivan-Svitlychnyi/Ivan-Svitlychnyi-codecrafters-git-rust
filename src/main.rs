8fc9d190eb8f8e7dbe0c5331e8ca57eb1f227f5b
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-01 15:03:49 +0300

    Clone 23

--- src/main.rs
+++ src/main.rs
@@ -440,6 +440,7 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
         println!("data: {:?}", &data);
         let tree_sha = data.clone().nth(data.count() -1).unwrap();
 
+
         println!("tree_sha: {}", &tree_sha);
         let path_f = target_dir.to_owned() + &format!("/.git/objects/{}/{}",&tree_sha[..2],&tree_sha[2..]);
         let (_, sha1_out) = write_tree(&path_f).unwrap();
