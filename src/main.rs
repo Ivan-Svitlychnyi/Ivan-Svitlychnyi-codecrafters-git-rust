2fa895bcebb87e9d05f1f298afa64195d799d3f9
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-02 13:47:48 +0300

    Clone 27

--- src/main.rs
+++ src/main.rs
@@ -479,7 +479,6 @@ fn clone_repo(args: &[String]) -> Result<String, io::Error> {
 
                 obj_write_data += &content;
                 //-----------------------
-
                 let mut hasher = Sha1::new();
                 hasher.update(obj_write_data.as_bytes());
                 let result = hasher.finalize();
