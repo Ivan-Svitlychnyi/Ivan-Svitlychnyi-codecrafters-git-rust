2c979f961f0a0aaf4feb9bc959b9059221d6c88c
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-03 21:18:00 +0300

    Clone 31

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,7 @@ fn  main() {
 
 fn git_init() -> Result<String, io::Error> {
     fs::create_dir(".git")?;
+    fs::create_dir(".git")?;
     fs::create_dir(".git/objects")?;
     fs::create_dir(".git/refs")?;
     fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
