c5016574b7ae30bc5e3039383f5352c78c1b480a
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-03 21:09:50 +0300

    Clone 31

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ use std::str::FromStr;
 use anyhow::{Context, Result};
 
 
-fn  main() ->Result<()> {
+fn  main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     // println!("Logs from your program will appear here!");
 
@@ -39,37 +39,37 @@ fn  main() ->Result<()> {
         }
       
       
-    } else if args[1] == "cat-file" && args[2] == "-p" {
-        print!(
-            "{}",
-            String::from_utf8(read_git_object(&args[3])?)?
-        );
-    } else if args[1] == "hash-object" && args[2] == "-w" {
-        let file_data = fs::read(args[3].to_string())?;
-        let (_, sha1_out) = write_hash_object(file_data, "blob")?;
+    // } else if args[1] == "cat-file" && args[2] == "-p" {
+    //     print!(
+    //         "{}",
+    //         String::from_utf8(read_git_object(&args[3])?)?
+    //     );
+    // } else if args[1] == "hash-object" && args[2] == "-w" {
+    //     let file_data = fs::read(args[3].to_string())?;
+    //     let (_, sha1_out) = write_hash_object(file_data, "blob")?;
 
-        println!("hash-object in: {}", sha1_out);
+    //     println!("hash-object in: {}", sha1_out);
 
-    } else if args[1] == "ls-tree" && args[2] == "--name-only" {
-        let result = read_tree(&args[3])?;
-        for s in result {
-            println!("{}", String::from_utf8(s)?);
-        }
+    // } else if args[1] == "ls-tree" && args[2] == "--name-only" {
+    //     let result = read_tree(&args[3])?;
+    //     for s in result {
+    //         println!("{}", String::from_utf8(s)?);
+    //     }
 
-    } else if args[1] == "write-tree" {
-        let (_, sha1_out) = write_tree(&".".to_string())?;
-        print!("{}", sha1_out);
+    // } else if args[1] == "write-tree" {
+    //     let (_, sha1_out) = write_tree(&".".to_string())?;
+    //     print!("{}", sha1_out);
 
-    } else if args[1] == "commit-tree" {
-        print!("{}", create_commit(&args)?);
+    // } else if args[1] == "commit-tree" {
+    //     print!("{}", create_commit(&args)?);
 
-    } else if args[1] == "clone" {
-        clone_repo(&args)?;
+    // } else if args[1] == "clone" {
+    //     clone_repo(&args)?;
 
     } else {
         println!("unknown command: {:#?}", args)
     }
-    Ok(())
+   // Ok(())
 
 }
 
