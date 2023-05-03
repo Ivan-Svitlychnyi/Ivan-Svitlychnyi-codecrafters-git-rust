60083ec82c4171d0c228158b2463c4b729b54481
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-03 21:16:58 +0300

    Clone 31

--- src/main.rs
+++ src/main.rs
@@ -31,54 +31,45 @@ fn  main() {
         panic!("enter the arguments!");
     }
     if args[1] == "init" {
+        git_init().unwrap();
+        // println!("{}", )
+    } else if args[1] == "cat-file" && args[2] == "-p" {
+        print!(
+            "{}",
+            String::from_utf8(read_git_object(&args[3]).unwrap()).unwrap()
+        );
+    } else if args[1] == "hash-object" && args[2] == "-w" {
+        let file_data = fs::read(args[3].to_string()).unwrap();
+        let (_, sha1_out) = write_hash_object(file_data, "blob").unwrap();
 
-        if let Err(err) =  git_init() {
-            eprintln!("ERROR: {}", err);
-           // err.chain().skip(1).for_each(|cause| eprintln!("because: {}", cause));
-            std::process::exit(1);
+        println!("hash-object in: {}", sha1_out);
+
+    } else if args[1] == "ls-tree" && args[2] == "--name-only" {
+        let result = read_tree(&args[3]).unwrap();
+        for s in result {
+            println!("{}", String::from_utf8(s).unwrap());
         }
-      
-      
-    // } else if args[1] == "cat-file" && args[2] == "-p" {
-    //     print!(
-    //         "{}",
-    //         String::from_utf8(read_git_object(&args[3])?)?
-    //     );
-    // } else if args[1] == "hash-object" && args[2] == "-w" {
-    //     let file_data = fs::read(args[3].to_string())?;
-    //     let (_, sha1_out) = write_hash_object(file_data, "blob")?;
 
-    //     println!("hash-object in: {}", sha1_out);
+    } else if args[1] == "write-tree" {
+        let (_, sha1_out) = write_tree(&".".to_string()).unwrap();
+        print!("{}", sha1_out);
 
-    // } else if args[1] == "ls-tree" && args[2] == "--name-only" {
-    //     let result = read_tree(&args[3])?;
-    //     for s in result {
-    //         println!("{}", String::from_utf8(s)?);
-    //     }
+    } else if args[1] == "commit-tree" {
+        print!("{}", create_commit(&args).unwrap());
 
-    // } else if args[1] == "write-tree" {
-    //     let (_, sha1_out) = write_tree(&".".to_string())?;
-    //     print!("{}", sha1_out);
-
-    // } else if args[1] == "commit-tree" {
-    //     print!("{}", create_commit(&args)?);
-
-    // } else if args[1] == "clone" {
-    //     clone_repo(&args)?;
+    } else if args[1] == "clone" {
+        clone_repo(&args).unwrap();
 
     } else {
         println!("unknown command: {:#?}", args)
     }
-   // Ok(())
-
 }
 
 
   
 
 
-fn git_init() -> Result<String> {
-    fs::create_dir(".git")?;
+fn git_init() -> Result<String, io::Error> {
     fs::create_dir(".git")?;
     fs::create_dir(".git/objects")?;
     fs::create_dir(".git/refs")?;
