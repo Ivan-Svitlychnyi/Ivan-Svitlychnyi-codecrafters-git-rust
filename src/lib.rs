3a8b7527744bfffa64e91a5e09382361b72e31ac
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-10 15:42:27 +0300

    Clone 35

--- src/lib.rs
+++ src/lib.rs
@@ -91,7 +91,7 @@ pub fn write_hash_object(file_data: &Vec<u8>, file_type: &str) -> Result<(Vec<u8
     store_vec.put_u8(' ' as u8);
     println!("store_vec ' ' = {:#?}", &store_vec);
     store_vec.put(file_data.len().to_be_bytes().as_slice());
-    println!("store_vec len()= {:#?}", &store_vec);
+    println!("store_vec len()= {:#?}", &file_data.len());
     store_vec.put_u8('\x00' as u8);
     println!("store_vec '\x00'= {:#?}", &store_vec);
     store_vec.put(file_data.as_slice());
