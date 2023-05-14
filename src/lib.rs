546da2855282834b071a2f88ff6afd64c9210769
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-14 18:39:22 +0300

    refactoring 4

--- src/lib.rs
+++ src/lib.rs
@@ -109,7 +109,7 @@ pub fn read_tree(file_path: &str) -> Result<Vec<Vec<u8>>, io::Error> {
 
     let mut result: Vec<Vec<u8>> = Vec::new();
     let mut start_byte = 0;
-    //println!("file_content = {:#?}", &String::from_utf8_lossy(&file_content[..]));
+   
     loop {
         if let Some(pos) = file_content[..].iter().position(|&r| r == '\x00' as u8) {
             let mut data_pos = file_content[start_byte..pos].split(|&r| r == ' ' as u8);
@@ -123,7 +123,7 @@ pub fn read_tree(file_path: &str) -> Result<Vec<Vec<u8>>, io::Error> {
                 start_byte = 0;  
             }
             file_content = file_content[pos + 1..].to_vec();
-
+            println!("file_content = {:#?}", &String::from_utf8_lossy(&file_content[..]));
         } else {
             break;
         }
