69993e85270216defb4180e486233b5222c4ecc8
Ivan Svitlychnyi <ivan.svitlychnyi@intellias.com>
2023-05-15 11:58:46 +0300

    refactoring 4

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,3 @@
-
 use clap::{Parser, Subcommand};
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -14,7 +13,6 @@ use clap::{arg, Args};
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
-
 }
 /*init ["/tmp/codecrafters-git-target/release/git-starter-rust", "init"]
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -49,7 +47,6 @@ pub enum Commands {
     Clone(CloneRepOptions),
 }
 
-
 //---Read a blob object args handling-----------------------------------------------------------------
 #[derive(Args)]
 pub struct ReadBlobOptions {
@@ -60,13 +57,12 @@ pub struct ReadBlobOptions {
 impl ReadBlobOptions {
     pub fn read(&self) -> Result<&str, ArgsReadError> {
         if let Some(blob_sha) = self.blob_sha.as_deref() {
-            if blob_sha.len() == 40{
-            return Ok(&blob_sha);
+            if blob_sha.len() == 40 {
+                return Ok(&blob_sha);
             }
         }
         Err(ArgsReadError::ReadBlobCommandError)
     }
-
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -82,9 +78,9 @@ impl CreateBlobOptions {
     pub fn read(&self) -> Result<&str, ArgsReadError> {
         if let Some(file_name) = self.file_name.as_deref() {
             let path = Path::new(file_name);
-            println!("path: {:?}", path);
-            if path.is_file(){
-            return Ok(&file_name);
+            //println!("path: {:?}", path);
+            if path.is_file() {
+                return Ok(&file_name);
             }
         }
         Err(ArgsReadError::CreateBlobCommandError)
@@ -101,9 +97,9 @@ pub struct ReadTreeOptions {
 
 impl ReadTreeOptions {
     pub fn read(&self) -> Result<&str, ArgsReadError> {
-        if let Some(tree_sha) = self.tree_sha.as_deref() {          
-            if tree_sha.len() == 40{
-            return Ok(&tree_sha);
+        if let Some(tree_sha) = self.tree_sha.as_deref() {
+            if tree_sha.len() == 40 {
+                return Ok(&tree_sha);
             }
         }
         Err(ArgsReadError::ReadTreeCommandError)
@@ -122,29 +118,26 @@ pub struct CommitTreeOptions {
 
 impl CommitTreeOptions {
     pub fn read(&self) -> Result<(&str, &str, &str), ArgsReadError> {
-       // println!("In read");
+        // println!("In read");
 
         if let Some(tree_sha) = self.tree_sha.as_deref() {
-           // println!("In read Some 1");
-            if let Some(commit_sha) = self.commit_sha.as_deref() {
-               // println!("In read Some 2");
-                if let Some(message) = self.message.as_deref() {
-                  //  println!("In read Some 3");
-                    return Ok((tree_sha, commit_sha, message));
+            if tree_sha.len() == 40 {
+                if let Some(commit_sha) = self.commit_sha.as_deref() {
+                    if commit_sha.len() == 40 {
+                        if let Some(message) = self.message.as_deref() {
+                            return Ok((tree_sha, commit_sha, message));
+                        }
+                            return Err(ArgsReadError::CommitTreeCommandErrorArgThree);
+                        
+                    }
                 }
-                else {
-                    return Err(ArgsReadError::CommitTreeCommandErrorArgThree);
+                return Err(ArgsReadError::CommitTreeCommandErrorArgTwo);
             }
-            }
-            else {
-            return Err(ArgsReadError::CommitTreeCommandErrorArgTwo);
         }
-        }
-        else {
         Err(ArgsReadError::CommitTreeCommandErrorArgOne)
     }
-    }
 }
+
 //-----------------------------------------------------------------------------------------------
 //-- Clone repo----------------------------------------------------------------------------------
 #[derive(Args)]
@@ -153,18 +146,17 @@ pub struct CloneRepOptions {
     dir: Option<String>,
 }
 
-impl CloneRepOptions{
+impl CloneRepOptions {
     pub fn read(&self) -> Result<(&str, &str), ArgsReadError> {
         if let Some(url) = self.url.as_deref() {
             if let Some(dir) = self.dir.as_deref() {
-                return Ok((url,dir));
-            }   
-           return  Err(ArgsReadError::CloneRepCommandErrorArgTwo);
+                return Ok((url, dir));
+            }
+            return Err(ArgsReadError::CloneRepCommandErrorArgTwo);
+        }
+        Err(ArgsReadError::CloneRepCommandErrorArgOne)
     }
-    Err(ArgsReadError::CloneRepCommandErrorArgOne)
 }
-}
-
 
 //-------------------------------------------------------------------------------------------
 //-----------Error handling------------------------------------------------------------------
@@ -178,7 +170,6 @@ pub enum ArgsReadError {
     CloneRepCommandErrorArgOne,
     CloneRepCommandErrorArgTwo,
 }
-
 
 impl Display for ArgsReadError {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
@@ -197,9 +188,13 @@ impl ArgsReadError {
             Self::ReadBlobCommandError => "ERROR: Read a blob object: blob sha not found",
             Self::CreateBlobCommandError => "ERROR: Create a blob objec: file name not found",
             Self::ReadTreeCommandError => "ERROR: Read a tree object: tree sha not found",
-            Self::CommitTreeCommandErrorArgOne=> "ERROR: Commit a tree object: tree sha not found",
-            Self::CommitTreeCommandErrorArgTwo=> "ERROR: Commit a tree object: commit sha not found",
-            Self::CommitTreeCommandErrorArgThree=> "ERROR: Commit a tree object: message not found",
+            Self::CommitTreeCommandErrorArgOne => "ERROR: Commit a tree object: tree sha not found",
+            Self::CommitTreeCommandErrorArgTwo => {
+                "ERROR: Commit a tree object: commit sha not found"
+            }
+            Self::CommitTreeCommandErrorArgThree => {
+                "ERROR: Commit a tree object: message not found"
+            }
             Self::CloneRepCommandErrorArgOne => "ERROR: Clone a repository: url not found",
             Self::CloneRepCommandErrorArgTwo => "ERROR: Clone a repository: dir not found",
         }
